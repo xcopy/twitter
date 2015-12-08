35f226a0f58050e5697eb3a68cc1947d6033a180
Kairat Jenishev <kairat.jenishev@gmail.com>
2015-12-08 20:15:28 +0600

    Devise views, able to signin via username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  # Virtual attribute for authenticating by either :username or :email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+
+    login = conditions.delete(:login)
+
+    if login
+      where(conditions).where(['lower(username) = :value OR lower(email) = :value', {value: login.downcase}]).first
+    else
+      where(conditions).first
+    end
+  end
 end
