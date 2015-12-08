555a09e76a9f8b2ed3ec1ba25f2d062d563451cb
Kairat Jenishev <kairat.jenishev@gmail.com>
2015-12-08 22:04:10 +0600

    Root path and devise path names changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
 
-  devise_for :users
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: :login,
+      sign_out: :logout,
+      sign_up: :signup
+    }
 end
