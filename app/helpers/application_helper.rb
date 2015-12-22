6473db59a3b9639bda9a7e4aab91e22c231fb16f
Kairat Jenishev <kairat.jenishev@gmail.com>
2015-12-22 19:57:01 +0600

    Modals as partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def close_modal
+    button_tag '&times;'.html_safe, type: :button, class: :close, data: {dismiss: :modal}
+  end
 end
